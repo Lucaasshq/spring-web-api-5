ab7a06473161ca1a1a316081d1d11c924fe7af09
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-24 18:12:30 -0300

    .

---
+++ src/main/java/com/LucasH/my_first_web_api/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.LucasH.my_first_web_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to My First Web API";
+    }
+}
