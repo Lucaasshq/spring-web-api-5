e25abdd2495911e594babc52a1f4cd2638339a3b
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-24 21:40:35 -0300

    refactor coding

--- src/main/java/com/LucasH/my_first_web_api/UsuarioRepository/UsuarioRepository.java
+++ src/main/java/com/LucasH/my_first_web_api/UsuarioRepository/UsuarioRepository.java
@@ -3,6 +3,7 @@ package com.LucasH.my_first_web_api.UsuarioRepository;
 import com.LucasH.my_first_web_api.Model.Usuario;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public class UsuarioRepository {
 
-    public void save(Usuario usuario)  {
+    public void save(Usuario usuario) {
         if (usuario.getId() == null) {
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         } else {
@@ -20,12 +21,12 @@ public class UsuarioRepository {
         System.out.println(usuario);
     }
 
-    public void deleteById(Integer id)  {
+    public void deleteById(Integer id) {
         System.out.printf("DELETE/id - Recebendo o id: %d para excluir um usuário%n", id);
         System.out.println(id);
     }
 
-    public List<Usuario> findAll()  {
+    public List<Usuario> findAll() {
         System.out.println("LIST - Listando os usuários do sistema");
         List<Usuario> usuarios = new ArrayList<>();
         usuarios.add(new Usuario("LucasH", "123456"));
@@ -43,4 +44,6 @@ public class UsuarioRepository {
         System.out.printf("FIND/username - Recebendo o username: %s para localizar um usuário%n", username);
         return new Usuario("LucasH", "123456");
     }
+
+
 }
--- src/main/java/com/LucasH/my_first_web_api/controller/UsuarioController.java
+++ src/main/java/com/LucasH/my_first_web_api/controller/UsuarioController.java
@@ -3,17 +3,29 @@ package com.LucasH.my_first_web_api.controller;
 import com.LucasH.my_first_web_api.Model.Usuario;
 import com.LucasH.my_first_web_api.UsuarioRepository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario> getUsuarios() {
        return usuarioRepository.findAll();
+    }
+@GetMapping("/{username}")
+    public Usuario getUsuario(@PathVariable("username") String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable("id") Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+    @PostMapping()
+    public void postUsuario(@RequestBody Usuario usuario) {
+        usuarioRepository.save(usuario);
     }
 }
